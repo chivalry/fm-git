8435fe73c4dc3847f8bb45a33309e9480e857a0f
Charles Ross <chivalry@mac.com>
2017-11-18 19:52:22 -0800

    Some refactoring

--- fm-git.py
+++ fm-git.py
@@ -11,30 +11,21 @@ logging.basicConfig(format='%(levelname)s %(asctime)s: %(message)s',
 def commit_to_repository(filename, repository, path, comment, username, password):
     '''Use parameters to commit the file to a git repository.
     
-    Create a back of the served file, move the backup to the repository folder,
-    prompt the user for a comment if one isn't provided, and commit the repository,
-    and push to the server'''
+    Create a backup of the served file, move the backup to the repository folder,
+    and commit the repository using the provided comment, and push to the server'''
 
-    # Fix any missing characters at the end of the parameters.
-    filename   = cond_append(filename, '.fmp12')
-    repository = cond_append(repository, '/')
-    path       = cond_append(path, '/')
-
+    filename = filename if filename.endswith('.fmp12') else filename + '.fmp12'
     startup_vol = startup_drive_name()
 
-    # Use absolute path, assuming applescript is in same directory.
-    cmd = 'osascript ' + os.path.dirname(os.path.realpath(__file__)) + '/create-ddr.applescript'
-    ddr_folder = subprocess.check_output(cmd, shell=True).splitlines()[0]
-    time.sleep(0.5)
-    shutil.move(os.path.expanduser("~/Desktop") + "/" + ddr_folder, repository + 'DDR/' + ddr_folder)
-
-    cmd = 'fmsadmin backup {} -d filemac:/{}{} -k 0 -u {} -p {}'
+    create_ddr(repository)
+    
+    cmd = 'fmsadmin backup {} --dest filemac:/{}{} --keep 0 --username {} --password {}'
     cmd = cmd.format(filename, startup_vol, repository, username, password)
     subprocess.call(cmd, shell=True)
 
-    db_path = repository + 'Databases/'
-    src_path = db_path + path + filename
-    trg_path = repository + filename
+    db_path = os.path.join(repository, 'Databases')
+    src_path = os.path.join(db_path, path, filename)
+    trg_path = os.path.join(repository, filename)
     shutil.move(src_path, trg_path)
     shutil.rmtree(db_path)
 
@@ -43,13 +34,32 @@ def commit_to_repository(filename, repository, path, comment, username, password
 
 def startup_drive_name():
     '''Use AppleScript to return the name of the startup drive.'''
+
     ascript = 'tell application "System Events" to get (name of startup disk)'
     cmd = "osascript -e '{}'".format(ascript)
     return subprocess.check_output(cmd, shell=True).splitlines()[0]
 
-def cond_append(string, suffix):
-    '''Append the suffix if it doesn't already end the string.'''
-    return string if string.endswith(suffix) else string + suffix
+def create_ddr(repository):
+    '''Use AppleScript to create the DDR and return the timestamp name.'''
+    
+    parent_folder = os.path.dirname(os.path.realpath(__file__))
+    ddr_script_path = os.path.join(parent_folder, 'create-ddr.applescript')
+    cmd = 'osascript ' + ddr_script_path
+    ddr_name = subprocess.check_output(cmd, shell=True).splitlines()[0]
+
+    # Move to repository's DDR folder.
+    src_ddr = os.path.join(os.path.expanduser("~/Desktop"), ddr_name)
+    time.sleep(0.25)
+    trg_ddr = os.path.join(repository, 'DDR')
+    move_files(src_ddr, trg_ddr)
+
+def move_files(src, dest):
+    '''Move all the files in src to dest and delete src.'''
+
+    for filename in os.listdir(src):
+        filepath = os.path.join(src, filename)
+        shutil.copy(filepath, dest)
+    shutil.rmtree(src)
 
 if __name__ == '__main__':
     if len(sys.argv) != 7:
